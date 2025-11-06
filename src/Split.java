ffafcf02a7b0cf4c6a30fb6d4ceda3407e2e7121
Shruti011106 <shruti.cs241313@global.org.in>
2025-11-06 11:32:37 -0800

    11/6/2025

---
+++ src/Split.java
@@ -0,0 +1,10 @@
+public class Split {
+    public static void main(String[] args) {
+        String str = "Hello Java";
+        String a[] = str.split(" ");
+
+        for (String str1 : a) {
+            System.out.printf(str1);
+        }
+    }
+}
