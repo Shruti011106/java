b717b656bb918e6967eafc24db506a5ed0d9412f
Shruti011106 <shruti.cs241313@global.org.in>
2025-11-10 12:40:41 -0800

    string

---
+++ src/string.java
@@ -0,0 +1,7 @@
+class main{
+    public static void main (String[] args){
+        StringBuilder str=new StringBuilder("hello" );
+    str.insert(2,"bye");
+        System.out.println(str);
+    }
+}
