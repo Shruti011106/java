44d2b18a2f8ff5dd19afaea8f15f43acd7692120
Shruti011106 <shruti.cs241313@global.org.in>
2025-11-20 13:35:48 -0800

    area

--- src/shapeofcircle.java
+++ src/shapeofcircle.java
@@ -1,41 +1,41 @@
-  abstract  class Shape{
-     abstract  void  area();
+abstract  class Shape{
+    abstract  void  area();
 
- }
- class circle extends Shape{
-     int r;
+}
+class circle extends Shape{
+    int r;
 
-     public circle(int r) {
-         this.r=r;
-     }
+    public circle(int r) {
+        this.r=r;
+    }
 
-     void area(){
-         double result=3.14*r*r;
-         System.out.println(result);
+    void area(){
+        double result=3.14*r*r;
+        System.out.println(result);
 
-     }
- }
- class Rectangle extends  Shape{
-     int length;
-     int b;
-     public Rectangle(int length,int b) {
-         this.length=length;
-         this.b=b;
-     }
-     void area(){
+    }
+}
+class Rectangle extends  Shape{
+    int length;
+    int b;
+    public Rectangle(int length,int b) {
+        this.length=length;
+        this.b=b;
+    }
+    void area(){
 
-         double result=length*b;
-         System.out.println(result);
+        double result=length*b;
+        System.out.println(result);
 
-     }
+    }
 
- }
- class hello{
-     public static void main(String[] args) {
-         circle s1=new circle(4);
-         s1.area();
-         Rectangle s2=new Rectangle(4, 5);
-         s2.area();
+}
+class hello{
+    public static void main(String[] args) {
+        circle s1=new circle(4);
+        s1.area();
+        Rectangle s2=new Rectangle(4, 5);
+        s2.area();
 
-     }
- }
+    }
+}
